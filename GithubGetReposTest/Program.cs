794d8d0b80ff7a2c590362c88878022bf7630c9f
Kubariev Ilya <polcrazpolcraz@gmail.com>
2021-01-03 18:14:11 +0200

    Добавьте файлы проекта.

---
+++ GithubGetReposTest/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace GithubGetReposTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var lol = Repositories.GetRepositories("https://api.github.com/users/gymnasy55/repos?sort=updated").Result;
+            foreach (var repository in lol) Console.WriteLine($"{repository.Name}: {repository.Url}");
+        }
+    }
+}
